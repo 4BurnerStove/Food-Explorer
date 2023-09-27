d3cad58a0d0647fe971d66cf0fb3867fb4e6e59f
Andre Morryson <amorrysson77@gmail.com>
2023-09-27 18:58:10 -0300

    Create users.routes and index.js, controller route

---
+++ Back-end/src/routes/users.routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+
+const usersRoutes = Router()
+
+const usersController = new UsersController()
+
+
+
+module.exports = usersRoutes
--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const { usersRoutes } = require('./routes/users.routes')
+
 
 const app = express()
+app.use(express.json())
 
 const PORT = 5555
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
