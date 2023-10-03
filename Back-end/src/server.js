ed14dba6ed9d3b6f5475cef5394248246ea1b0b8
Andre Morryson <amorrysson77@gmail.com>
2023-10-03 10:34:47 -0300

    create a bd connection at server

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,5 +1,6 @@
 require('express-async-errors')
 const AppError = require('./utils/AppError')
+const database = require('./database/sqlite')
 
 const express = require('express')
 
@@ -7,7 +8,11 @@ const routes = require('./routes')
 
 const app = express()
 app.use(express.json())
+
 app.use(routes)
+
+database()
+
 
 app.use((error, req, res, next) => {
   if(error instanceof AppError) {
