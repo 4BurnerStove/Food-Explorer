dc1ab6cea5f2fbe8fcc49cb2cc4ffb110eb0cc3a
Andre Morryson <amorrysson77@gmail.com>
2023-10-23 12:55:51 -0300

    Re-generating database structure

--- Back-end/src/database/knex/migrations/20231003140444_users.js
+++ Back-end/src/database/knex/migrations/20231003140444_users.js
@@ -1,9 +1,11 @@
 exports.up = knex => knex.schema.createTable('users', table => {
   table.increments('id')
-  table.text('name')
-  table.text('email')
-  table.text('password')
-  table.boolean('isAdmin').defaultTo(false)
+  table.text('name').notNullable()
+  table.text('email').notNullable()
+  table.text('password').notNullable()
+
+  table.enum("role", ["admin", "customer"], {useNative: true, enumName: 'roles'}).notNullable().default("customer")
+
 
 })
 exports.down = knex => knex.schema.dropTable('users')
