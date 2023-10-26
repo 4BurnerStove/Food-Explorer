9f606ce8d08fbff4cc70d957838ab485b94a0ca7
Andre Morryson <amorrysson77@gmail.com>
2023-10-26 06:53:11 -0300

    Create sessionsController and add sesions routes

--- Back-end/src/routes/index.js
+++ Back-end/src/routes/index.js
@@ -4,10 +4,12 @@ const usersRouter = require('./users.routes')
 const dishesRoutes = require('./dishes.routes')
 const favoritesRoutes = require('./favorites.routes')
 const ingredientsRoutes = require('./ingredients.routes')
+const sessionsRoutes = require('./sessions.routes')
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRoutes)
 routes.use('/dishes', dishesRoutes)
 routes.use('/favorites', favoritesRoutes)
 routes.use('/ingredients', ingredientsRoutes)
