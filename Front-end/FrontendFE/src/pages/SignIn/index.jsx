1f57fb524da8835a0d83b6493bbc76ce348b613d
Andre Morryson <amorrysson77@gmail.com>
2023-11-01 19:27:53 -0300

    Stylizate component button

--- Front-end/FrontendFE/src/pages/SignIn/index.jsx
+++ Front-end/FrontendFE/src/pages/SignIn/index.jsx
@@ -1,9 +1,11 @@
 import { Container } from "./styles"
+import { Button } from "../../components/Button"
+
 
 export function SignIn() {
   return (
     <Container>
-     
+      <Button name='Entrar'></Button>
     </Container>
   )
 }
