fd7a38be1cda94afc5efdadaa632874614db1b61
Andre Morryson <amorrysson77@gmail.com>
2023-11-01 19:33:18 -0300

    add globalStyles for project

--- Front-end/FrontendFE/src/main.jsx
+++ Front-end/FrontendFE/src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-
+import GlobalStyles from './styles/global'
 import Theme from './styles/theme'
 
 import { SignIn } from "./pages/SignIn"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   
-      <ThemeProvider theme={Theme}>
-        <SignIn />
-      </ThemeProvider>
-
-  </React.StrictMode>,
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      <SignIn />
+    </ThemeProvider>
+  </React.StrictMode >,
 )
