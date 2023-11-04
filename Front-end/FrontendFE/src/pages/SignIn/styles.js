1722b5f15b0a2f3a6520c13190ba1c052c5940a0
Andre Morryson <amorrysson77@gmail.com>
2023-11-04 15:02:15 -0300

    Finalizate signIn page

--- Front-end/FrontendFE/src/pages/SignIn/styles.js
+++ Front-end/FrontendFE/src/pages/SignIn/styles.js
@@ -1,11 +1,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  padding: 10rem 13.6rem;
+  padding: 10rem 18.6rem;
   display: flex;
   justify-content: center;
-  text-align: center; 
-
 
  color: ${({theme}) => theme.COLORS.LIGHT_100};
 
@@ -20,17 +18,15 @@ export const Container = styled.div`
 
 export const Form = styled.form`
   width: 90rem;
-  margin: 0 auto;
   padding: 6.4rem;
   background-color: ${({theme}) => theme.COLORS.DARK_700};
   border-radius: 2rem;
 
   > section {
-    label {
-      width: 100%;
-      color: ${({theme}) => theme.COLORS.LIGHT_400};
-    }
+    margin-block: 3.2rem;
+  }
 
+  button {
     margin-block: 3.2rem;
   }
 
