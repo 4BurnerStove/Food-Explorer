bd6e31a0c4d0730d1bb82e63efbb440ae5ff6897
Andre Morryson <amorrysson77@gmail.com>
2023-11-15 14:37:00 -0300

    Create text-area and structure for page NewDishe

--- Front-end/FrontendFE/src/pages/CreateDishes/index.jsx
+++ Front-end/FrontendFE/src/pages/CreateDishes/index.jsx
@@ -1,6 +1,10 @@
-import { Container } from './styles'
+import { Container, Form, Content } from './styles'
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
+import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
+import { ButtonText } from '../../components/ButtonText';
+import { Button } from '../../components/Button'
 import { SlArrowLeft } from "react-icons/sl";
 
 
@@ -8,8 +12,51 @@ export function CreateDishe() {
   return (
     <Container>
       <Header />
-      <ButtonText icon={SlArrowLeft} name={'Voltar'}></ButtonText>
+      <Form>
+        <ButtonText icon={SlArrowLeft} name={'Voltar'}></ButtonText>
 
+        <Content>
+          <section>
+            <h1>Adicionar prato</h1>
+          </section>
+
+          <div className='divPrinf'>
+            <div id='imageDishe' className='SectionForms'>
+              <p>Imagem do prato</p>
+              <Input></Input>
+            </div>
+
+            <div id='Name' className='SectionForms'>
+              <p>Nome</p>
+              <Input placeholder='Ex: Salada Martirs'></Input>
+            </div>
+
+            <div id='Categories' className='SectionForms'>
+              <p>Categoria</p>
+              <Input></Input>
+            </div>
+          </div>
+
+          <div className='divPrinf'>
+            <div id='Ingredients' className='SectionForms'>
+              <p>Ingredientes</p>
+              <Input></Input>
+            </div>
+
+            <div id='Price' className='SectionForms'>
+              <p>Preço</p>
+              <Input placeholder='R$ 00,00'></Input>
+            </div>
+          </div>
+
+          <div className='SectionForms'>
+            <p>Descrição</p>
+            <Textarea placeholder='Fale brevemente sobre o prato, seus ingredientes e composição'></Textarea>
+          </div>
+
+          <Button name='Salvar alterações'></Button>
+        </Content>
+      </Form>
       <Footer />
     </Container>
   )
--- Front-end/FrontendFE/src/pages/CreateDishes/styles.js
+++ Front-end/FrontendFE/src/pages/CreateDishes/styles.js
@@ -2,6 +2,84 @@ import styled from "styled-components"
 
 
 export const Container = styled.div`
-  
+  display: flex;
+  color: ${({theme}) => theme.COLORS.LIGHT_100};
+`
 
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  > button {
+    display: flex;
+    justify-content: flex-end;
+    white-space: nowrap;
+    margin-top: 3.2rem;
+    width: 25%;
+  }
+
+  div {
+    display: flex;
+    gap: .8rem;
+  }
+
+  h1 {
+    font-size: 3.2rem;
+    font-weight: 500;
+    margin-top: 2.4rem;
+    margin-bottom: 3.2rem;
+  }
+`
+
+export const Form = styled.form`
+  width: 80%;
+  height: 50vh;
+  
+  margin: 19.5rem auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  .divPrinf {
+    width: 100%;
+    display: flex;
+    gap: 3.2rem;
+
+
+    #imageDishe {
+      width: 25%;
+    }
+
+    #Name {
+      width: 55%;
+    }
+
+    #Categories {
+      width: 40%;
+    }
+
+    #Ingredients {
+      width: 70%;
+    }
+
+    #Price {
+      width: 30%;
+    }
+  }
+
+  .SectionForms {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    
+    p {
+      margin-bottom: 1.6rem;
+    }
+  }
+
+  > button {
+    justify-content: flex-start;
+  }
 `
